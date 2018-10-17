3a175f252fe8bf4f3cec360b1113bcfd8d45d3f0
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-16 17:44:21 -0700

    introtorandom

--- src/random/IntroToRandom.java
+++ src/random/IntroToRandom.java
@@ -13,16 +13,16 @@ public class IntroToRandom {
 		//3. Now make r random. 
 		//   Create an object of the Random class
 		//	 use .nextInt() to get a random number
-		
-
+		Random number = new Random();
+		r = number.nextInt(3);
 		//4. Limit the random number between 0 and 100
-
+r = number.nextInt(75-25+1)+25;
 		//5. Limit the random number between 25 and 75. Hint: ((highest value - lowest value)+1) + lowest value
 
 		//6. Challenge: Limit the random number between -222 and 88
-
+r = number.nextInt()
 		//1. Print out the value of r
-		
+		System.out.println(r);
 		//2. Run the program.  What number appears in the console?
 		//   Run it again. Is the number the same?
 	}
